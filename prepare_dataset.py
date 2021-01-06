8c350219fca067067bbd597a906336db790e9ae4
Giovanni De Toni <giovanni.det@gmail.com>
2021-01-06 16:14:04 +0100

    Add simple script to polish the Rakuten dataset.
    It performs simple actions (e.g, tokenization, lowercase the tokens
    remove punctuation marks, etc.). It save the result into a Python
    pickle compressed through gzip.

---
+++ prepare_dataset.py
@@ -0,0 +1,60 @@
+import argparse
+import string
+import pandas as pd
+
+# Punctuation table
+table = str.maketrans(dict.fromkeys(string.punctuation))
+
+def strip_punctuation(product_title):
+    """
+    Given a string, it removes the punctuation signs as defined
+    in string.punctuation
+    :param product_title: string
+    :return: string without punctuation
+    """
+    return product_title.translate(table)
+
+
+def convert_number_to_token(product_tokenized):
+    """
+    Given a tokenized string, it will convert all the numeric occurrences
+    into a custom token (@NUMBER@).
+    :param product_tokenized: the tokenized product title
+    :return: sanitized product (list)
+    """
+    return list(map(lambda x: "@NUMBER@" if x.isnumeric() else x, product_tokenized))
+
+
+if __name__ == "__main__":
+
+    # Build the command-line parser
+    parser = argparse.ArgumentParser("prepare_dataset.py",
+                                     description="Perform some basic cleaning on Rakuten dataset.")
+    parser.add_argument("--data", help="Path to the .tsv file containing the data",
+                        type=str, default="./dataset/rdc-catalog-train.tsv")
+    parser.add_argument("--output", help="Path where to save the polished data",
+                        type=str, default="./dataset/rdc-catalog-train-polished.tsv.gzip")
+
+    # Parse and get the arguments
+    args = parser.parse_args()
+    data_path = args.data
+    output_path = args.output
+
+    # Read the data inside a dataframe
+    df = pd.read_csv(data_path, header=None, sep="\t", names=["product", "category"])
+
+    # We polish the data given, namely the "product" column. The step we perform are
+    # the following:
+    # 1) Put everything to lowercase
+    # 2) Remove punctuation marks
+    # 3) Tokenize the string
+    # 4) Convert numbers with a specific token @NUMBER@. We keep unchanged
+    #    alphanumeric entries (e.g, 3343als)
+    df["product"] = df["product"].apply(lambda x: x.lower())
+    df["product"] = df["product"].apply(lambda x: strip_punctuation(x))
+    df["product"] = df["product"].apply(lambda x: x.split())
+    df["product"] = df["product"].apply(lambda x: convert_number_to_token(x))
+
+    # Save the polished dataset on disk
+    df.to_pickle(output_path, compression="gzip")
+
