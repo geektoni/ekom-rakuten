b0112515f8e888edb85eca6045889c2ac943baf4
Giovanni De Toni <giovanni.det@gmail.com>
2021-01-07 18:06:05 +0100

    Update prepare_dataset.py.
    This script will generate automatically also the training and test splitting.

--- prepare_dataset.py
+++ prepare_dataset.py
@@ -4,6 +4,8 @@ import csv
 import pandas as pd
 import numpy as np
 
+from sklearn.model_selection import train_test_split
+
 # Punctuation table
 table = str.maketrans(dict.fromkeys(string.punctuation))
 
@@ -46,8 +48,10 @@ if __name__ == "__main__":
                                      description="Perform some basic cleaning on Rakuten dataset.")
     parser.add_argument("--data", help="Path to the .tsv file containing the data",
                         type=str, default="./dataset/rdc-catalog-train.tsv")
-    parser.add_argument("--output", help="Path where to save the polished data",
-                        type=str, default="./dataset/rdc-catalog-train-polished.tsv")
+    parser.add_argument("--output-train", help="Path where to save the polished data",
+                        type=str, default="./dataset/train_dataset.tsv.gzip")
+    parser.add_argument("--output-test", help="Path where to save the polished data",
+                        type=str, default="./dataset/test_dataset.tsv.gzip")
     parser.add_argument("--fasttext", help="Prepare the dataset for Fasttext",
                         action="store_true", default=False)
     parser.add_argument("--size", help="Specify the size of the final dataset",
@@ -58,7 +62,8 @@ if __name__ == "__main__":
     # Parse and get the arguments
     args = parser.parse_args()
     data_path = args.data
-    output_path = args.output
+    output_train = args.output_train
+    output_test = args.output_test
     fasttext = args.fasttext
     size = args.size
     stratified = args.stratified
@@ -82,14 +87,20 @@ if __name__ == "__main__":
     df["product"] = df["product"].apply(lambda x: x.split())
     df["product"] = df["product"].apply(lambda x: convert_number_to_token(x))
 
-    # Save the polished dataset on disk
-    df.to_pickle(output_path+".gzip", compression="gzip")
+    # Split the dataset into train/test
+    train, test = train_test_split(df, test_size=0.2)
+    train = train.reset_index(drop=True)
+    test = test.reset_index(drop=True)
+
+    # Save the train and test sets into pickles
+    train.to_pickle(output_train, compression="gzip")
+    test.to_pickle(output_test, compression="gzip")
 
     if fasttext:
         # Fasttext dataset format expects lines in the following fashion:
         # __label__<category>  <sentence>
-        df["category"] = df["category"].apply(lambda x: "__label__" + x)
-        df[['category', 'product']].to_csv(output_path + ".fasttext.csv",
+        train["category"] = train["category"].apply(lambda x: "__label__" + x)
+        train[['category', 'product']].to_csv(output_train + ".fasttext.csv",
                                            index=False,
                                            sep=' ',
                                            header=None,
