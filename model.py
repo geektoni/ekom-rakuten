108bac276756bea7b6c4e6a7e2d49c9649b3c9a8
Giovanni De Toni <giovanni.det@gmail.com>
2021-01-07 15:35:48 +0100

    Add our model class.
    It will use a simple LSTM with dropout and a linear layer to obtain
    the final class.

---
+++ model.py
@@ -0,0 +1,19 @@
+import torch
+
+class DefaultLSTM(torch.nn.Module):
+
+    def __init__(self, n_categories, embedding_size=100, hidden_size=25, drop_prob=0.2):
+        super(DefaultLSTM, self).__init__()
+
+        self.embedding_size = embedding_size
+        self.hidden_size = hidden_size
+        self.n_categories = n_categories
+
+        self.lstm = torch.nn.LSTM(self.embedding_size, hidden_size=hidden_size)
+        self.drop = torch.nn.Dropout(p=drop_prob)
+        self.out = torch.nn.Linear(self.hidden_size, self.n_categories)
+
+    def forward(self, batch):
+
+        lstm_out, h = self.lstm(batch)
+        return self.out(self.drop(lstm_out))
